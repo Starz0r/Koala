10a403931f1266e002072a518156c12bbb10976d
Starz0r <starz0rdesign@gmail.com>
2018-01-11 03:20:53 -0600

    Basic Command Line Arguments and Parameter passing

--- Koala/App.xaml.cs
+++ Koala/App.xaml.cs
@@ -3,12 +3,17 @@
 using Koala.Services;
 
 using Windows.ApplicationModel.Activation;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace Koala
 {
     public sealed partial class App : Application
     {
+        public static string CmdArgv { get; set; }
+
         private Lazy<ActivationService> _activationService;
 
         private ActivationService ActivationService
@@ -34,7 +39,38 @@ namespace Koala
 
         protected override async void OnActivated(IActivatedEventArgs args)
         {
+            // Handle CommandLineLaunch
+            if (args.Kind == ActivationKind.CommandLineLaunch)
+            {
+                var commandLine = args as CommandLineActivatedEventArgs;
+                if (commandLine != null)
+                {
+                    CmdArgv = commandLine.Operation.Arguments;
+                }
+            }
+
+            // Return Control
             await ActivationService.ActivateAsync(args);
+
+            // Resume Normally
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                rootFrame.NavigationFailed += OnNavigationFailed;
+                Window.Current.Content = rootFrame;
+            }
+
+            rootFrame.Navigate(typeof(Views.MainPage), "OnActivated");
+
+            Window.Current.Activate();
+            base.OnActivated(args);
+        }
+
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
         private ActivationService CreateActivationService()
